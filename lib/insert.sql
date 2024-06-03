eae9a04aeda61677f88c85816181ba680ffd31c1
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-06-03 09:24:06 +0300

    organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 14, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 22, "F", "White", "cool", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 20, "F", "Dark brown", "nice", 0);
+INSERT INTO bears VALUES (5,"Melissa", 17, "F", "black", "calm", 1);
+INSERT INTO bears VALUES (6,"Grinch", 23, "M", "Brown", "intense", 1);
+INSERT INTO bears VALUES (7,"Wendy", 16, "F", "white", "goofy", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "calm", 1);
